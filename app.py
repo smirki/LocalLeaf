74d75fc7de70876b256a2dd2f01975e783da0335
aslanka <49008222+aslanka@users.noreply.github.com>
2023-04-09 01:21:23 -0400

    landing and sign up done

--- app.py
+++ app.py
@@ -2,7 +2,13 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
+
+
+
+
 @app.route('/')
+@app.route('/index')
 def home():
     return render_template('index.html')
 
