9a6726b61486897bdf0fb350138cc34d77083865
vrishankg <vrishankg@gmail.com>
2023-04-08 20:53:24 -0400

    added ui

--- app.py
+++ app.py
@@ -6,6 +6,18 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+	return render_template('auth/sign-up.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    account_type = request.form['account-type']
+    return f'Thank you for signing up as a {account_type}!'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
